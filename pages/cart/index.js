d445e6211dcd32fd64bebced88ace94acddd1ddc
gongyuh <2736498007@qq.com>
2020-10-13 20:53:23 +0800

    第三次提交

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -1,66 +1,41 @@
-// pages/cart/index.js
+import { getSetting, chooseAddress, openSetting, showModal ,showToast} from "../../utils/asyncWx.js";
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    address: {},
+    cart: [],
+    allChecked: false,
+    totalPrice: 0,
+    totalNum: 0
   },
+  onShow(){
+    // 1 获取缓存中的收货地址信息
+    const address = wx.getStorageSync("address");
+    // 1 获取缓存中的购物车数据
+    const cart = wx.getStorageSync("cart") || [];
+    
+    this.setData({ address });
+    this.setData({cart})
+  },  
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+  // 点击 收货地址
+  async handleChooseAddress() {
+    try {
+      // 1 获取 权限状态
+      const res1 = await getSetting();
+      const scopeAddress = res1.authSetting["scope.address"];
+      // 2 判断 权限状态
+      if (scopeAddress === false) {
+        await openSetting();
+      }
+      // 4 调用获取收货地址的 api
+      let address = await chooseAddress();
+      address.all = address.provinceName + address.cityName + address.countyName + address.detailInfo;
 
-  },
+      // 5 存入到缓存中
+      wx.setStorageSync("address", address);
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    } catch (error) {
+      console.log(error);
+    }
   }
 })
